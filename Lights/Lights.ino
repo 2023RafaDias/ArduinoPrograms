fcd531126fc871f1101207b3fad44dcaeb270f61
2023RafaDias <2023rafaeladias@gmail.com>
2021-10-23 16:43:29 -0300

    not working

---
+++ Lights/Lights.ino
@@ -0,0 +1,16 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
